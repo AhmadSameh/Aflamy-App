a28639c51767ac30ec0a83fae69beb3d3d021f71
Ahmad Sameh <ahmadsameh@ahmads-mac.local>
2019-08-06 22:49:35 +0200

    Initial Commit

---
+++ Aflamy App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aflamy App
+//
+//  Created by Ahmad Sameh on 8/6/19.
+//  Copyright © 2019 Ahmad Sameh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
